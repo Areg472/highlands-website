70060f310866f548bb7322d5f004fc6452e43033
Lexy <lexy@lexy.boo>
2026-06-21 01:29:53 +1000

    chore: remove unused google fonts
    Signed-off-by: Lexy <lexy@lexy.boo>

--- app/layout.js
+++ app/layout.js
@@ -1,15 +1,4 @@
-import { Roboto_Mono, Roboto_Slab } from "next/font/google";
 import "./globals.css";
-
-const robotoSans = Roboto_Slab({
-  variable: "--font-roboto-slab",
-  subsets: ["latin"],
-});
-
-const robotoMono = Roboto_Mono({
-  variable: "--font-roboto-mono",
-  subsets: ["latin"],
-});
 
 export const metadata = {
   title: "Highlands YSWS!",
@@ -18,10 +7,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html
-      lang="en"
-      className={`${robotoSans.variable} ${robotoMono.variable} h-full antialiased`}
-    >
+    <html lang="en" className={`h-full antialiased`}>
       <body className="min-h-full flex flex-col">{children}</body>
     </html>
   );
