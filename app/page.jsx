d39def40738c7b19e2c3feddb822e5659c96daf9
Areg472 <aregazatyank@outlook.com>
2026-06-22 11:47:39 +0400

    moboile optimization

--- app/page.jsx
+++ app/page.jsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function Home() {
   return (
     <div className="flex flex-col bg-gray-700 font-sans justify-center items-center min-h-screen">
-      <div className="text-center bg-yellow-500 border-2 border-black rounded-2xl w-250 mt-16 p-6 justify-center">
+      <div className="text-center bg-yellow-500 border-2 border-black rounded-2xl w-80 md:w-250 mt-16 p-6 justify-center">
         <h1 className="text-black text-5xl">Welcome to Highlands YSWS!!! ⛰️</h1>
         <p className="text-black text-2xl mt-4">
           Check out #highlands on Slack and scroll down for more info!
@@ -17,9 +17,9 @@ export default function Home() {
         RSVP Now!
       </Link>
 
-      <div className="text-center bg-green-400 border-2 border-black rounded-2xl w-175 content-center items-center mt-16 p-4 justify-center hover:scale-110 transition-all">
+      <div className="text-center bg-green-400 border-2 border-black rounded-2xl w-80 md:w-175 content-center items-center mt-16 p-4 justify-center hover:scale-110 transition-all">
         <h1 className="text-black text-5xl">What&#39;s this about?</h1>
-        <p className="text-black bg-blue-300 text-2xl w-125 text-center mt-4 p-4 border-2 border-black rounded-2xl justify-center items-center mx-auto">
+        <p className="text-black bg-blue-300 text-2xl w-30 md:w-125 text-center mt-4 p-4 border-2 border-black rounded-2xl justify-center items-center mx-auto">
           This is a mountain themed YSWS where you code hours and can earn
           prizes like earbuds, credits, games, 3d printers, and more! Plus
           we&#39;ll have hiking related stuff as prizes! And if you code for
@@ -28,9 +28,9 @@ export default function Home() {
         </p>
       </div>
 
-      <div className="text-center bg-green-400 border-2 border-black rounded-2xl w-175 content-center items-center mt-16 p-4 justify-center hover:scale-110 transition-all">
+      <div className="text-center bg-green-400 border-2 border-black rounded-2xl w-80 md:w-175 content-center items-center mt-16 p-4 justify-center hover:scale-110 transition-all">
         <h1 className="text-black text-5xl">Where is Armenia?</h1>
-        <p className="text-black bg-blue-300 text-2xl w-125 text-center mt-4 p-4 border-2 border-black rounded-2xl justify-center items-center mx-auto">
+        <p className="text-black bg-blue-300 text-2xl w-30 md:w-125 text-center mt-4 p-4 border-2 border-black rounded-2xl justify-center items-center mx-auto">
           Armenia is in the Southern Caucasus, between Iran, Georgia, and
           Turkey! Armenia is considered the 10th most mountainous nation, hence
           the name &#34;Highlands.&#34; I would like this YSWS to happen in
@@ -40,9 +40,9 @@ export default function Home() {
         </p>
       </div>
 
-      <div className="text-center bg-green-400 border-2 border-black rounded-2xl w-175 content-center items-center mt-16 p-4 justify-center hover:scale-110 mb-10 transition-all">
+      <div className="text-center bg-green-400 border-2 border-black rounded-2xl w-80 md:w-175 content-center items-center mt-16 p-4 justify-center hover:scale-110 mb-10 transition-all">
         <h1 className="text-black text-5xl">Is there anything in Yerevan?</h1>
-        <p className="text-black bg-blue-300 text-2xl w-125 text-center mt-4 p-4 border-2 border-black rounded-2xl justify-center items-center mx-auto">
+        <p className="text-black bg-blue-300 text-2xl w-30 md:w-125 text-center mt-4 p-4 border-2 border-black rounded-2xl justify-center items-center mx-auto">
           There are a lot of places to visit in Yerevan. Most famous sites in
           Yerevan include the cascade complex, Tsitsernakaberd, Republic Square,
           The National History Museum, the Opera, and even the Metro here + more
